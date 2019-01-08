9ade245d81763427839dcca4c97e0b5cac4da5c9
Christian Egwim <emeksense@gmail.com>
2019-01-08 16:43:14 +0100

    Route parameters

--- third-app-building-RESful-API/index.js
+++ third-app-building-RESful-API/index.js
@@ -18,7 +18,22 @@ app.get('/api/courses', (req, res) => {
 });
 
 
-// 
+// to get a specific couse by its Id:
+app.get('/api/courses/:id', (req, res) => {
+
+    // to read the id we use the params method:
+    res.send(req.params.id);
+});
+
+//To return year and month.
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
+//This is to read value of a query string eg. /api/posts/2018?name=emeksense
+app.get('/api/posts/:year/', (req, res) => {
+    res.send(req.query);
+});
 
 //Using enviroment variable to set port dinamically
 const port = process.env.PORT || 3210;
