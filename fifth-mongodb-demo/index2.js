89e134e4945eefb49da270d5dccd5a1872c6157f
Christian Egwim <emeksense@gmail.com>
2019-01-17 11:14:18 +0100

    Validation: Built-IN and Also Custom Validations

---
+++ fifth-mongodb-demo/index2.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+mongoose
+    .connect('mongodb://localhost/playground', {
+        useNewUrlParser: true
+    })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Validation implementation
+const courseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+    caterory: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile', 'network']
+    },
+    author: String,
+    // Custom Validation
+    tags: {
+        type: Array,
+        validate: {
+            validator: function (v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should have at least one tag ooh!!!'
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    isPublished: Boolean,
+    price: {
+        type: Number,
+        required: function () {
+            return this.isPublished;
+        },
+        min: 10,
+        max: 200
+
+    }
+
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        //name: 'Test Course',
+        caterory: 'database',
+        author: 'Christian Egwim',
+        tags: [],
+        isPublished: true
+    });
+
+    try {
+        const result = await course.save();
+        console.log(result);
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+createCourse();
